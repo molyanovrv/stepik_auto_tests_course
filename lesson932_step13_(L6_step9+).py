a46f2593de6f02023cbdea9634646037c48a3f62
my name <molyanovrv@gmail.com>
2023-01-07 14:58:47 +0300

    из lesson6_step9 переделано на unittest

--- lesson932_step13_(L6_step9+).py
+++ lesson932_step13_(L6_step9+).py
@@ -1,18 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import unittest
 
-try: 
-    link = "http://suninjuly.github.io/registration1.html"
-    #link = "http://suninjuly.github.io/registration2.html"
-    browser = webdriver.Chrome()
+def scenario(link):
     browser.get(link)
+    time.sleep(1)
 
-    # Ваш код, который заполняет обязательные поля
     browser.find_element(By.CSS_SELECTOR, "div.first_block input.first").send_keys("Ivan")
     browser.find_element(By.CSS_SELECTOR, "div.first_block input.second").send_keys("Ivanov")
     browser.find_element(By.CSS_SELECTOR, "div.first_block input.third").send_keys("Ivan@ov.ru")
-
     time.sleep(2)
 
     # Отправляем заполненную форму
@@ -26,13 +23,29 @@ try:
     # находим элемент, содержащий текст
     welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
     # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
+    h1 = welcome_text_elt.text
+    return h1
 
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
 
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    #time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+class TestText(unittest.TestCase):
+ def test_regForm1(self):
+    link = "http://suninjuly.github.io/registration1.html"
+    h1 = scenario(link)
+    print(h1)
+    print("Congratulations! You have successfully registered!" == h1)
+    print("===")
+    self.assertEqual("Congratulations! You have successfully registered!", h1, f'Has no congrat on {link}')
+
+ def test_regForm2(self):
+    link = "http://suninjuly.github.io/registration2.html"
+    h1 = scenario(link)
+    print(h1)
+    print("Congratulations! You have successfully registered!" == h1)
+    print("===")
+    self.assertEqual("Congratulations! You have successfully registered!", h1, f'Has no congrat on {link}')
+
+#main prog: 
+browser = webdriver.Chrome()
+unittest.main()
+# закрываем браузер после всех манипуляций
+browser.quit()
